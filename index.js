f5ebeb6eefa45d94f1ff241329880a53a9b95eeb
Rafael Amaral <rafaelbruno86@gmail.com>
2016-11-07 14:47:35 -0300

    Added PUT and DELETE

--- index.js
+++ index.js
@@ -1,43 +1,81 @@
 const express = require('express');
-const fs = require('fs')
-const _ = require('lodash')
+const fs = require('fs');
+const path = require('path');
+const _ = require('lodash');
 const engines = require('consolidate');
+const bodyParser = require('body-parser');
 
 const app = express();
 const users = []
 
-fs.readFile('users.json', {encoding: 'utf8'}, (err, data) => {
-  if (err) throw err
+function getUserFilePath (username) {
+  return path.join(__dirname, 'users', username) + '.json';
+}
 
-  JSON.parse(data).forEach((user) => {
-    user.name.full = _.startCase(`${user.name.first} ${user.name.last}`);
-    users.push(user);
+function getUser (username) {
+  const user = JSON.parse(fs.readFileSync(getUserFilePath(username), {encoding: 'utf8'}));
+  user.name.full = _.startCase(`${user.name.first} ${user.name.last}`);
+  _.keys(user.location).forEach(function (key) {
+    user.location[key] = _.startCase(user.location[key]);
   })
+  return user;
+}
 
-})
+function saveUser (username, data) {
+  const fp = getUserFilePath(username);
+  fs.unlinkSync(fp); // delete the file
+  fs.writeFileSync(fp, JSON.stringify(data, null, 2), {encoding: 'utf8'});
+}
 
 app.engine('hbs', engines.handlebars)
+
 app.set('views', './views');
 app.set('view engine', 'hbs');
+
 app.use('/profilepics', express.static('images'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/favicon.ico', function (req, res) {
+  res.end();
+})
 
 app.get('/', (req, res) => {
-  res.render('index', {users: users});
-})
-
-app.get(/big.*/, (req, res, next) => {
-  console.log('BIG USER ACCESS');
-  next();
-})
-
-app.get(/.*dog.*/, (req, res, next) => {
-  console.log('DOGS GO WOOF');
-  next();
+  const users = [];
+  fs.readdir('users', (err, files) => {
+    files.forEach((file) => {
+      fs.readFile(path.join(__dirname, 'users', file), {encoding: 'utf8'}, (err, data) => {
+        const user = JSON.parse(data);
+        user.name.full = _.startCase(`${user.name.first} ${user.name.last}`);
+        users.push(user);
+        if (users.length === files.length) {
+          res.render('index', {users: users});
+        }
+      })
+    })
+  })
 })
 
 app.get('/:username', (req, res) => {
   const username = req.params.username;
-  res.render('user', {username: username})
+  const user = getUser(username);
+  res.render('user', {
+    user: user,
+    address: user.location
+  });
+})
+
+app.put('/:username', (req, res) => {
+  const username = req.params.username;
+  const user = getUser(username);
+  user.location = req.body;
+  saveUser(username, user);
+  res.end();
+})
+
+app.delete('/:username', (req, res) => {
+  const fp = getUserFilePath(req.params.username);
+  fs.unlinkSync(fp); // delete the file
+  res.sendStatus(200);
 })
 
 const server = app.listen(3000, () => {
